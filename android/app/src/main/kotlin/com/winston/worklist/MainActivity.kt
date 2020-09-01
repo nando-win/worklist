fa662fffb4e6b07b0c48635eb640ec4ac78f5e07
Fernando <fernando.wcb@gmail.com>
2020-08-31 21:08:58 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/winston/worklist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.winston.worklist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Main(),
+  ));
+}
+
+class Main extends StatefulWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
