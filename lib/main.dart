49bd8f7d72b6e53f0dc6c6e99587df5929d41ea3
Fernando Winston <fernando.wcbgmail.com>
2020-11-10 21:20:07 -0300

    Created snack function

--- lib/main.dart
+++ lib/main.dart
@@ -63,10 +63,11 @@ class _HomeState extends State<Home> {
                       labelStyle: TextStyle(color: Colors.blueAccent)),
                 )),
                 RaisedButton(
-                    onPressed: _addToDo,
-                    color: Colors.blueAccent,
-                    child: Text('ADD'),
-                    textColor: Colors.white)
+                  onPressed: _addToDo,
+                  color: Colors.blueAccent,
+                  child: Text('ADD'),
+                  textColor: Colors.white,
+                )
               ],
             ),
           ),
@@ -74,23 +75,38 @@ class _HomeState extends State<Home> {
               child: ListView.builder(
                   padding: EdgeInsets.only(top: 10.0),
                   itemCount: _toDoList.length,
-                  itemBuilder: (context, index) {
-                    return CheckboxListTile(
-                      title: Text(_toDoList[index]['title']),
-                      value: _toDoList[index]['OK'],
-                      secondary: CircleAvatar(
-                        child: Icon(
-                            _toDoList[index]['OK'] ? Icons.check : Icons.error),
-                      ),
-                      onChanged: (ok) {
-                        setState(() {
-                          _toDoList[index]['OK'] = ok;
-                          _saveData();
-                        });
-                      },
-                    );
-                  }))
+                  itemBuilder: buildItem))
         ],
+      ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]['title']),
+        value: _toDoList[index]['OK'],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]['OK'] ? Icons.check : Icons.error),
+        ),
+        onChanged: (ok) {
+          setState(() {
+            _toDoList[index]['OK'] = ok;
+            _saveData();
+          });
+        },
       ),
     );
   }
